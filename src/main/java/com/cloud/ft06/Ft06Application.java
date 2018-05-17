46e02b610d8810a1e0ae4bcb6870f2ca3af781d4
Piyush Jain <piyush@Piyushs-MBP.lan>
2018-05-17 23:24:50 +0800

    Intial commit

---
+++ src/main/java/com/cloud/ft06/Ft06Application.java
@@ -0,0 +1,12 @@
+package com.cloud.ft06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ft06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ft06Application.class, args);
+	}
+}
