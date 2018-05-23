4271091c66e2928003be5d2db041169f7d38d1c8
Piyush Jain <piyush@Piyushs-MacBook-Pro.local>
2018-05-23 22:02:52 +0800

    Initial upload function implemented

---
+++ src/main/java/com/cloud/ft06/BController.java
@@ -0,0 +1,27 @@
+package com.cloud.ft06;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/storage")
+public class BController {
+
+    private AmazonClient amazonClient;
+
+    @Autowired
+    BController(AmazonClient amazonClient) {
+        this.amazonClient = amazonClient;
+    }
+
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
+        return this.amazonClient.uploadFile(file);
+    }
+
+    @DeleteMapping("/deleteFile")
+    public String deleteFile(@RequestPart(value = "url") String fileUrl) {
+        return this.amazonClient.deleteFileFromS3Bucket(fileUrl);
+    }
+}
