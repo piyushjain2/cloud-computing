8182224f9818516f482610a0869c99bd38e65189
Piyush Jain <piyush.jain03@u.nus.edu>
2018-05-26 14:32:08 +0800

    Add user pojo

---
+++ src/main/java/com/cloud/ft06/user/User.java
@@ -0,0 +1,31 @@
+package com.cloud.ft06.user;
+
+public class User {
+    private String username;
+    private String userEmail;
+    private String userId;
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
